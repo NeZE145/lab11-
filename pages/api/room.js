7789aee3842d79cef502bf2f38e689347ed2e4a0
NeZE <boss-NeZE@hotmail.com>
2022-09-29 15:38:33 +0700

    11

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,14 +2,24 @@ import { checkToken } from "../../backendLibs/checkToken";
 import { readChatRoomsDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
-  const user = checkToken(req);
-  if (!user) {
-    return res.status(401).json({
-      ok: false,
-      message: "Yon don't permission to access this api",
+  if (req.method === "GET") {
+    const user = checkToken(req);
+    if (!user) {
+      return res.status(401).json({
+        ok: false,
+        message: "Yon don't permission to access this api",
+      });
+    }
+    const chatrooms = readChatRoomsDB();
+    //create room data and return response
+
+    const room = chatrooms.map((x) => ({
+      roomId: x.roomId,
+      roomName: x.roomName,
+    }));
+    return res.json({
+      ok: true,
+      room,
     });
   }
-  const chatrooms = readChatRoomsDB();
-
-  //create room data and return response
 }
